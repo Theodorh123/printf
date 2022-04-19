3ca3c18558c521b35b3df84199e9457e68d6bf1e
Rhomeinel <rhomeinel@gmail.com>
2022-04-19 09:33:27 -0700

    Add task 0 & _putchar prototype

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* struct stdout_ - structure for symbol and function
+* @sym: The operator
+* @pfunc: The function assoiated
+*/
+typedef struct stdout_
+{
+	char sym;
+	int (*pfunc)(va_list*);
+} stdout_t;
+
+int _putchar(char c);
+stdout_t select_printer(char s);
+int _printf(const char *format, ...)
+
+
+
+
+
+
+#endif /* MAIN_H */
