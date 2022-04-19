fba8bd20117a814d2008c7f77695437b29d43d24
Rhomeinel <rhomeinel@gmail.com>
2022-04-19 16:05:39 -0700

    Add general prototypes

--- main.h
+++ main.h
@@ -1,22 +1,30 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 #include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
 #include <stdio.h>
 
 /**
-* struct stdout_ - structure for symbol and function
-* @sym: The operator
-* @pfunc: The function assoiated
-*/
-typedef struct stdout_
+ *struct print - structure for different types of cases
+ * struct print - is the struct for printer functions
+ * @f: is the pointer to a printer functions
+ * @oper: is the identifier
+ *
+ * Description: struct stores pointers to the
+ * printer functions.
+ */
+typedef struct print
 {
-	char sym;
-	int (*pfunc)(va_list *);
-} stdout_t;
+	char *oper;
+	int (*f)(va_list y);
+} print_t;
 
-int _putchar(char c);
-stdout_t select_printer(char s);
+/* General Prototypes */
+
 int _printf(const char *format, ...);
+int print_func(int *i, const char *format, va_list arguments);
 
-
-#endif /* MAIN_H */
+#endif
